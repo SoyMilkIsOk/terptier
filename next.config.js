673552b68a2e79dca34062a3ea04d63f86ef8125
Sam S <sammi12006@gmail.com>
2025-06-23 22:17:18 -0600

    push for imgs

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
+  images: {
+    remotePatterns: [new URL('https://elnwy0xndspvgnal.public.blob.vercel-storage.com/**')],
+  },
 };
 
 module.exports = nextConfig;
