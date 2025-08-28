c9713cb703e989279f5cfff63e0ee1d172c862f5
Sam S. <me@samsteingard.com>
2025-08-27 21:35:13 -0600

    Add migration converting strainSlug to text

--- prisma/migrations/20250826120000_add_strain_slug/migration.sql
+++ prisma/migrations/20250826120000_add_strain_slug/migration.sql
@@ -1,2 +1,3 @@
-ALTER TABLE "Strain" ADD COLUMN "strainSlug" TEXT NOT NULL DEFAULT gen_random_uuid();
+-- Add strainSlug column to Strain
+ALTER TABLE "Strain" ADD COLUMN "strainSlug" SERIAL;
 CREATE UNIQUE INDEX "Strain_strainSlug_key" ON "Strain"("strainSlug");
